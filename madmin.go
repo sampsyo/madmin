d0104902093edc2edccb4d3d3577b49321983887
Adrian Sampson <adrian@radbox.org>
2014-02-01 21:41:49 -0800

    go fmt

--- madmin.go
+++ madmin.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "github.com/sampsyo/madmin/mailbox"
-    "fmt"
-    "os"
-    "log"
+	"fmt"
+	"github.com/sampsyo/madmin/mailbox"
+	"log"
+	"os"
 )
 
 func trymail(host string, user string, pass string) {
-    conn, err := mailbox.Connect(host, user, pass)
-    if (err != nil) {
-        log.Fatal("connection failed")
-    }
-    messages, err := conn.Messages("INBOX", 5)
-    if (err != nil) {
-        log.Fatal("failed to get messages", err.Error())
-    }
-    for _, mm := range messages {
-        fmt.Println(mm.Subject)
-    }
-    conn.Close()
+	conn, err := mailbox.Connect(host, user, pass)
+	if err != nil {
+		log.Fatal("connection failed")
+	}
+	messages, err := conn.Messages("INBOX", 5)
+	if err != nil {
+		log.Fatal("failed to get messages", err.Error())
+	}
+	for _, mm := range messages {
+		fmt.Println(mm.Subject)
+	}
+	conn.Close()
 }
 
 func main() {
-    trymail(os.Args[1], os.Args[2], os.Args[3])
+	trymail(os.Args[1], os.Args[2], os.Args[3])
 }
--- mailbox/mailbox.go
+++ mailbox/mailbox.go
@@ -3,116 +3,116 @@
 package mailbox
 
 import (
-    "code.google.com/p/go-imap/go1/imap"
-    "net/mail"
-    "bytes"
-    "errors"
-    "time"
+	"bytes"
+	"code.google.com/p/go-imap/go1/imap"
+	"errors"
+	"net/mail"
+	"time"
 )
 
 // A connection to an IMAP server.
 type Connection struct {
-    client *imap.Client
+	client *imap.Client
 }
 
 // The metadata for an email message. Includes "envelope" information but not
 // the body of the message.
 type MessageMeta struct {
-    // The message's unique identifier.
-    UID     uint32
+	// The message's unique identifier.
+	UID uint32
 
-    // The subject line.
-    Subject string
+	// The subject line.
+	Subject string
 
-    // The message's total size in bytes.
-    Size    uint32
+	// The message's total size in bytes.
+	Size uint32
 }
 
 func (conn *Connection) connect(host string) error {
-    var err error
-    conn.client, err = imap.DialTLS("imap.gmail.com", nil)
-    return err
+	var err error
+	conn.client, err = imap.DialTLS("imap.gmail.com", nil)
+	return err
 }
 
 func (conn *Connection) login(user string, pass string) error {
-    if conn.client.State() != imap.Login {
-        return errors.New("not ready to log in")
-    }
-    _, err := conn.client.Login(user, pass)
-    return err
+	if conn.client.State() != imap.Login {
+		return errors.New("not ready to log in")
+	}
+	_, err := conn.client.Login(user, pass)
+	return err
 }
 
 // Open a new connection to an IMAP server.
 func Connect(host string, user string, pass string) (*Connection, error) {
-    conn := new(Connection)
+	conn := new(Connection)
 
-    if err := conn.connect(host); err != nil {
-        return nil, err
-    }
+	if err := conn.connect(host); err != nil {
+		return nil, err
+	}
 
-    if err := conn.login(user, pass); err != nil {
-        return nil, err
-    }
+	if err := conn.login(user, pass); err != nil {
+		return nil, err
+	}
 
-    return conn, nil
+	return conn, nil
 }
 
 // Log out from the IMAP server and close the connection.
 func (conn *Connection) Close() {
-    conn.client.Logout(3)
+	conn.client.Logout(3)
 }
 
 // Retrieve metadata for all messages in a given mailbox.
 func (conn *Connection) Messages(box string, count uint32) ([]*MessageMeta,
-error) {
-    conn.client.Select(box, true)
+	error) {
+	conn.client.Select(box, true)
 
-    // Create a range set that selects the most recent `count` messages (or
-    // all messages if the mailbox contains fewer than that). Also, allocate a
-    // slice for the results.
-    set, _ := imap.NewSeqSet("")
-    var messages []*MessageMeta
-    totalCount := conn.client.Mailbox.Messages
-    if totalCount >= count {
-        set.AddRange(totalCount - (count - 1), totalCount)
-        messages = make([]*MessageMeta, count)
-    } else {
-        set.Add("1:*")
-        messages = make([]*MessageMeta, totalCount)
-    }
+	// Create a range set that selects the most recent `count` messages (or
+	// all messages if the mailbox contains fewer than that). Also, allocate a
+	// slice for the results.
+	set, _ := imap.NewSeqSet("")
+	var messages []*MessageMeta
+	totalCount := conn.client.Mailbox.Messages
+	if totalCount >= count {
+		set.AddRange(totalCount-(count-1), totalCount)
+		messages = make([]*MessageMeta, count)
+	} else {
+		set.Add("1:*")
+		messages = make([]*MessageMeta, totalCount)
+	}
 
-    // Fetch messages.
-    cmd, err := conn.client.Fetch(set, "RFC822.HEADER RFC822.SIZE UID")
-    if (err != nil) {
-        return nil, err
-    }
+	// Fetch messages.
+	cmd, err := conn.client.Fetch(set, "RFC822.HEADER RFC822.SIZE UID")
+	if err != nil {
+		return nil, err
+	}
 
-    // Parse each message.
-    i := 0
-    for cmd.InProgress() {
-        err = conn.client.Recv(time.Second * 5)
-        if (err != nil) {
-            return nil, err
-        }
+	// Parse each message.
+	i := 0
+	for cmd.InProgress() {
+		err = conn.client.Recv(time.Second * 5)
+		if err != nil {
+			return nil, err
+		}
 
-        // Process the message data received so far.
-        for _, rsp := range cmd.Data {
-            msgInfo := rsp.MessageInfo()
-            header := imap.AsBytes(msgInfo.Attrs["RFC822.HEADER"])
-            msg, err := mail.ReadMessage(bytes.NewReader(header))
-            if (err != nil) {
-                return nil, err
-            }
+		// Process the message data received so far.
+		for _, rsp := range cmd.Data {
+			msgInfo := rsp.MessageInfo()
+			header := imap.AsBytes(msgInfo.Attrs["RFC822.HEADER"])
+			msg, err := mail.ReadMessage(bytes.NewReader(header))
+			if err != nil {
+				return nil, err
+			}
 
-            messages[i] = &MessageMeta{
-                UID: msgInfo.UID,
-                Size: msgInfo.Size,
-                Subject: msg.Header.Get("Subject"),
-            }
-            i++
-        }
-        cmd.Data = nil
-    }
+			messages[i] = &MessageMeta{
+				UID:     msgInfo.UID,
+				Size:    msgInfo.Size,
+				Subject: msg.Header.Get("Subject"),
+			}
+			i++
+		}
+		cmd.Data = nil
+	}
 
-    return messages, nil
+	return messages, nil
 }
